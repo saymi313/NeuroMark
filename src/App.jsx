63f08d18a24b3f8a3e8e463f7dac73dacee18628
Esha-tahir123 <115134241+Esha-tahir123@users.noreply.github.com>
2025-05-06 02:11:26 -0700

    Update App.jsx
    Added Routes for Login and Signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+
+import LoginForm from './Components/Login/LoginForm';
+import SignUpForm from './Components/Signup/SignupForm';
 import Dashboard from './Components/Dashboard/Dashboard';
 import HomePage from './Pages/Home'
-import Analytics from './Pages/Analytics';
 import Settings from './Pages/Settings';
-import './App.css';
-
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Analytics from './Pages/Anaytics';
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />}>
-        <Route index element={<HomePage />} />
-        <Route path="stats" element={<Analytics />} />
-        <Route path="settings" element={<Settings />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
+<div>
+  <Routes>
+<Route path="/" element= { <SignUpForm/>}/>
+ <Route path='/Login' element={ <LoginForm/>}/>
+ <Route path="/Dashboard" element={<Dashboard/>}>
+ <Route index element={<HomePage />} />
+ <Route path="stats" element={<Analytics />} />
+<Route path="settings" element={<Settings />} />
+</Route>
+<Route path="*" element={<Navigate to="/" replace />} /> 
+  </Routes>
+ </div>
+    );
 }
 
 export default App;
